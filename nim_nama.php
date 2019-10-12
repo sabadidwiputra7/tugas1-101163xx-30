9ef6b99a9340a766e6aba4c3e9e039f70898f2e8
iamo <50961295+sabadidwiputra7@users.noreply.github.com>
2019-10-12 07:19:15 +0700

    Add files via upload

---
+++ nim_nama.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>Perkenalan</title>
+</head>
+<body background="bg.jpg">
+<h2><center> Komputasi awan  </center></h2>
+<?php
+
+echo "<br>";
+echo "<center>";
+echo "<font size='14'  font='comic sans' > NIM		: 10116314 </font> ";
+echo "<br>";
+
+echo "<font size='14' > NAMA	: Sabadi Dwiputra </font> ";
+?>
+
+</body>
+</html>
